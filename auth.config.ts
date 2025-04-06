585b90865aaaa746130f3d4dc67cd37ba5980d86
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-07 00:04:32 +0530

    feat: initialize project with PostgreSQL, Prisma, and authentication setup
    - Added components.json for configuration.
    - Implemented SignIn button component for user authentication.
    - Created reusable Button component with variants and styles.
    - Set up Docker Compose for PostgreSQL service.
    - Established Prisma client for database interactions.
    - Added utility function for class name merging.
    - Configured middleware for NextAuth authentication.
    - Updated package.json with necessary dependencies for authentication and UI components.
    - Created initial Prisma migration for user authentication tables.
    - Defined Prisma schema for User, Account, Session, VerificationToken, and Authenticator models.
    - Updated TypeScript configuration for improved type checking and module resolution.

---
+++ auth.config.ts
@@ -0,0 +1,19 @@
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [Google, GitHub],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
