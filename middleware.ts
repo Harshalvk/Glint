50f845cfb284c6145474820c652ca8ffd9c4c00f
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-09 23:33:28 +0530

    feat: implement middleware for route protection and session management

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,35 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const protectedRoutes = ["/dashboard"];
+const authRoutes = ["/signin", "signup"];
+
+export const { auth } = NextAuth(authConfig);
+
+export default async function middleware(request: NextRequest) {
+  const { nextUrl } = request;
+
+  const isProtectedRoute = protectedRoutes.some(
+    (route) =>
+      route === nextUrl.pathname || nextUrl.pathname.startsWith(`${route}/`)
+  );
+
+  const isAuthRoute = authRoutes.some((route) => route === nextUrl.pathname);
+
+  const session = await auth();
+
+  if (!session && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/signin", nextUrl));
+  }
+
+  if (session && isAuthRoute) {
+    return NextResponse.redirect(new URL("/dashboard", nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [...protectedRoutes, ...authRoutes],
+};
