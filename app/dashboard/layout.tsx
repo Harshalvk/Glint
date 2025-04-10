70a1c7af5635a2ec83ce432c6a11cb398c89d1cb
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-11 02:04:50 +0530

    feat: integrate Credits component and update SignOut button in Layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,6 @@
+import { signOut } from "@/auth";
+import SignOutBtn from "@/components/auth/SignOutBtn";
+import Credits from "@/components/Credits";
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -8,13 +11,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen w-full flex-col items-center overflow-y-scroll px-6 py-6">
       <nav className="flex w-full items-center justify-between pb-6">
         <div className="flex items-center gap-3">
-          <p className="font-semibold tracking-tight capitalize">1 credit left</p>
+          <Credits />
           <Link href={"/dashboard/pricing"}>
-            <Button className="rounded-full" variant={"secondary"}>Buy more</Button>
+            <Button className="rounded-full" variant={"secondary"}>
+              Buy more
+            </Button>
           </Link>
-          <Link href={"/signout"}>
-            <Button className="rounded-full" variant={"secondary"}>Sign out</Button>
-          </Link>
+          <SignOutBtn />
         </div>
 
         <ModeToggle />
