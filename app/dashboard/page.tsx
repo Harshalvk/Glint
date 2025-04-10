217fb87034bd43158e8f36c9f6227c67b0176102
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-11 02:05:19 +0530

    feat: update page component to fetch user data and display ThumbnailCreator

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import GetUser from "@/actions/user/getUser";
 import React from "react";
+import ThumbnailCreator from "./_components/ThumbnailCreator";
+import Styles from "./_components/Styles";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  const user = await GetUser();
+  return (
+    <div className="flex flex-col gap-4 my-12 p-6">
+      <ThumbnailCreator />
+    </div>
+  );
 };
 
 export default page;
