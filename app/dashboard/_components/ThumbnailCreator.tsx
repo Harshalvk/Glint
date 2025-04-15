d7c411abcc399e60f90866a6e6f445abd179cad9
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-15 18:09:06 +0530

    feat: enhance ThumbnailCreator with loading state and image preview functionality

--- app/dashboard/_components/ThumbnailCreator.tsx
+++ app/dashboard/_components/ThumbnailCreator.tsx
@@ -3,43 +3,72 @@
 import React, { useState } from "react";
 import Dropzone from "./Dropzone";
 import Styles from "./Styles";
+import { Loader } from "lucide-react";
 
 const ThumbnailCreator = () => {
   const [selectedStyle, setSelectedStyle] = useState("style1");
-  const setSelectedImage = async (file?: File) => {};
+  const [loading, setLoading] = useState(false);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const setSelectedImage = async (file?: File) => {
+    if (file) {
+      setLoading(true);
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const src = e.target?.result as string;
+        setImageSrc(src);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
-    <div>
-      <div>
-        <p className="scroll-m-20 text-4xl font-bold tracking-tight">
-          Hi there
-        </p>
-        <p className="scroll-m-20 text-4xl font-bold tracking-tight">
-          Want to create a thumbnail?
-        </p>
-        <p className="leading-7 text-muted-foreground mt-2">
-          Use one of the templates below
-        </p>
-      </div>
-      <div className="mt-10 flex flex-col items-center justify-between gap-10 md:flex-row md:items-start">
-        <Styles
-          image="/styletest.png"
-          selectStyle={() => setSelectedStyle("style1")}
-          isSelected={selectedStyle === "style1"}
-        />
-        <Styles
-          image="/styletest.png"
-          selectStyle={() => setSelectedStyle("style2")}
-          isSelected={selectedStyle === "style2"}
-        />
-        <Styles
-          image="/styletest.png"
-          selectStyle={() => setSelectedStyle("style3")}
-          isSelected={selectedStyle === "style3"}
-        />
-      </div>
-      <Dropzone setSelectedImage={setSelectedImage} />
-    </div>
+    <>
+      {imageSrc ? (
+        <>
+          {loading && (
+            <div className="h-96 flex items-center justify-center">
+              <Loader
+                className="text-white animate-spin transition-all"
+                size={30}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <div>
+          <div>
+            <p className="scroll-m-20 text-4xl font-bold tracking-tight">
+              Hi there
+            </p>
+            <p className="scroll-m-20 text-4xl font-bold tracking-tight">
+              Want to create a thumbnail?
+            </p>
+            <p className="leading-7 text-muted-foreground mt-2">
+              Use one of the templates below
+            </p>
+          </div>
+          <div className="mt-10 flex flex-col items-center justify-between gap-10 md:flex-row md:items-start">
+            <Styles
+              image="/styletest.png"
+              selectStyle={() => setSelectedStyle("style1")}
+              isSelected={selectedStyle === "style1"}
+            />
+            <Styles
+              image="/styletest.png"
+              selectStyle={() => setSelectedStyle("style2")}
+              isSelected={selectedStyle === "style2"}
+            />
+            <Styles
+              image="/styletest.png"
+              selectStyle={() => setSelectedStyle("style3")}
+              isSelected={selectedStyle === "style3"}
+            />
+          </div>
+          <Dropzone setSelectedImage={setSelectedImage} />
+        </div>
+      )}
+    </>
   );
 };
 
