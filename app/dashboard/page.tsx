e81c5354186a5dc87e3fd281436e9b72eb5766e7
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-16 22:23:51 +0530

    feat: update page component to conditionally render content based on user credits

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,37 @@
-import GetUser from "@/actions/user/getUser";
 import React from "react";
 import ThumbnailCreator from "./_components/ThumbnailCreator";
-import Styles from "./_components/Styles";
+import GetUserCredits from "@/actions/user/getUserCredits";
+import Recent from "./_components/Recent";
+import { Button } from "@/components/ui/button";
+import BuyCreditsBtn from "./_components/BuyCreditsBtn";
 
 const page = async () => {
-  const user = await GetUser();
+  const userCredits = await GetUserCredits();
   return (
     <div className="flex flex-col gap-4 my-12 p-6">
-      <ThumbnailCreator />
+      {userCredits === 10 ? (
+        <>
+          <div>
+            <p className="scroll-m-20 text-4xl font-bold tracking-tight">
+              Hi there
+            </p>
+            <p className="scroll-m-20 text-4xl font-bold tracking-tight">
+              Want to create a thumbnail?
+            </p>
+            <p className="leading-7 text-muted-foreground mt-2">
+              Buy more credits to continue generate more thumbnails.
+            </p>
+            <BuyCreditsBtn className="mt-3"/>
+          </div>
+          <div className="mt-8">
+            <Recent />
+          </div>
+        </>
+      ) : (
+        <ThumbnailCreator>
+          <Recent />
+        </ThumbnailCreator>
+      )}
     </div>
   );
 };
